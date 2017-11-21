34c2e2184bfaa47cc8b13aa4d346cf64893f2f06
yemi <zcyemi@gmail.com>
2017-11-21 09:26:26 +0800

    update CommandBuffer and RenderTextureIdentifier

--- RigelSharp/RigelCore/Rendering/RenderTextureIdentifier.cs
+++ RigelSharp/RigelCore/Rendering/RenderTextureIdentifier.cs
@@ -4,11 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SharpDX;
+using SharpDX.Direct3D11;
+
 namespace RigelCore.Rendering
 {
-    public enum RenderTextureIdentifier
+    public enum InternalRenderTextureIdentifier
     {
         Default,
-        DepthStencilView
+        DepthStencilView,
+        RenderTargetView,
+    }
+
+    public class RenderTextureIdentifier
+    {
+        private Texture2D m_tex;
+
+        public InternalRenderTextureIdentifier m_identifier = InternalRenderTextureIdentifier.Default;
+
+        public RenderTextureIdentifier(Texture2D texture)
+        {
+            m_tex = texture;
+        }
+
+        public RenderTextureIdentifier(InternalRenderTextureIdentifier identifier)
+        {
+            m_identifier = identifier;
+            m_tex = null;
+        }
+
+        public T GetRawRenderTexture<T>(GraphicsContext context) where T: ResourceView
+        {
+            switch (m_identifier)
+            {
+                case InternalRenderTextureIdentifier.RenderTargetView:
+                    return context.DefaultRenderTargetView as T;
+                case InternalRenderTextureIdentifier.DepthStencilView:
+                    return context.DefaultDepthStencilView as T;
+            }
+
+            return null;
+        }
+
+        public static readonly RenderTextureIdentifier DefaultRenderTargetView = new RenderTextureIdentifier(InternalRenderTextureIdentifier.RenderTargetView);
+        public static readonly RenderTextureIdentifier DefaultDepthStencilView = new RenderTextureIdentifier(InternalRenderTextureIdentifier.DepthStencilView);
     }
 }
