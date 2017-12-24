d928b84a40dc1b9f53482d6514c0868f83913f75
yemi <zcyemi@gmail.com>
2017-12-24 09:51:37 +0800

    add projects

---
+++ RigelSharp/RigelCore/AssemblyLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+
+namespace Rigel
+{
+    public static class AssemblyLoader
+    {
+        public static void LoadAssemblies(string folderpath)
+        {
+            DirectoryInfo dir = new DirectoryInfo(folderpath);
+            if (!dir.Exists) return;
+        }
+    }
+}
