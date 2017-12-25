197e8359f8353f67b9589eaeb10bf2b83c86c054
yemi <zcyemi@gmail.com>
2017-12-25 09:51:28 +0800

    add OpenTkTest

---
+++ RigelSharp/OpenTKtest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTKtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ RigelSharp/Rigel.OpenTK/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Graphics;
+using OpenTK.Platform.Windows;
+
+namespace Rigel.OpenTK
+{
+    class Test
+    {
+
+    }
+}
